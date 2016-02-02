687e9bc0110018ae2de42439f759cb7ad70af311
Anton <novemberdevelop@gmail.com>
2016-02-02 09:09:44 +0400

    Update createMinimalDB.sh

--- SQL/createMinimalDB.sh
+++ SQL/createMinimalDB.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-USER=root
-PASS=651065
+USER=username
+PASS=yourpass
 mysql --user="$USER" --password=$PASS -e "CREATE DATABASE IF NOT EXISTS mindb CHARACTER SET utf8 COLLATE utf8_general_ci;"
 mysql --user="$USER" --password=$PASS mindb < mysqlDB.sql
 
